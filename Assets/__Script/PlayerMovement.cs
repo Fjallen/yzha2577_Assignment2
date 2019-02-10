9fc3f55e7fdea68c5d0ccedb3279cd4373c263aa
Fjallen <38442956+Fjallen@users.noreply.github.com>
2019-02-09 21:12:29 -0500

    Boxes and Player Implemented

---
+++ Assets/__Script/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float playerSpd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSpd = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)){
+            move += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.A)) { 
+            move += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            move += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            move += Vector3.right;
+        }
+        gameObject.transform.position = move * playerSpd + gameObject.transform.position;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
